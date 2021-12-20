ef2bb3621571c1f653bc1c8e59e106b72c64b66b
DevDisco <diederik.slob@gmail.com>
2021-12-20 15:29:58 +0100

    added extra api class to custom.php

--- apps/marsha/Custom.php
+++ apps/marsha/Custom.php
@@ -12,35 +12,61 @@ class Custom
 
     public function execute(string $table, int $id): bool|array
     {
-        
+
         $category = $this->getCategory();
 
         if ($table === "werk" && $category) {
 
             return $this->getSelection($category);
-        }
-        else if ($table === "werk" && !$id) {
+        } else if ($table === "werk" && !$id) {
 
             return $this->getShowcases();
-        }   
-        else if ($table === "werk" && $id > 0 ){
+        } else if ($table === "werk" && $id > 0) {
 
             return $this->getItem($id);
-        } 
-        else if ($table === "tekst" && !$id) {
+        } else if ($table === "tekst" && !$id) {
 
             return $this->getText();
-        } 
+        } else if ($table === "agenda" && !$id) {
+
+            return $this->getAgenda();
+        } else if ($table === "agenda" && $id) {
+
+            return $this->getAgendaItem($id);
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    private function getAgendaItem(int $id): bool|array
+    {
+        $sql = "SELECT * FROM agenda WHERE id=:id LIMIT 1";
+
+        $params = [':id' => $id];
+
+        return $this->database->read($sql, $params);
+    }
+
+
+    private function getAgenda(): bool|array
+    {
+        $sql = "SELECT *,id FROM agenda WHERE NOW() > in_agenda && NOW() < uit_agenda";
+
+        return $this->database->read($sql, [], PDO::FETCH_UNIQUE | PDO::FETCH_ASSOC);
     }
 
 
     private function getText(): bool|array
     {
-        $sql = "SELECT tag,tekst FROM `tekst`";
-        return $this->database->read($sql,[], PDO::FETCH_KEY_PAIR);
+        $sql = "SELECT tag,tekst,id,tag FROM tekst";
+
+        $array[] = $this->database->read($sql, [], PDO::FETCH_UNIQUE | PDO::FETCH_ASSOC);
+        return $array;
     }
 
-    private function getItem( int $id ): bool|array
+    private function getItem(int $id): bool|array
     {
         $sql = "SELECT * FROM `werk` WHERE id=:id LIMIT 1";
 
@@ -51,7 +77,7 @@ class Custom
 
     private function getShowcases(): array
     {
-        $result[CAT_KEY_NEW] = $this->getSelection(CAT_KEY_NEW);
+        $result[CAT_KEY_NEW] = $this->getSelection(CAT_KEY_NEW, 3);
         $result[CAT_KEY_AUTONOMOUS] = $this->getSelection(CAT_KEY_AUTONOMOUS, 3);
         $result[CAT_KEY_APPLIED] = $this->getSelection(CAT_KEY_APPLIED, 3);
         $result[CAT_KEY_ARCHIVE] = $this->getSelection(CAT_KEY_ARCHIVE, 3);
@@ -61,7 +87,7 @@ class Custom
     private function getSelection(string $category, int|bool $n = false): bool|array
     {
 
-        $limit = $n === false ? " ORDER BY titel" : " ORDER BY RAND() LIMIT ".$n;
+        $limit = $n === false ? " ORDER BY titel" : " AND in_overzicht=1 ORDER BY RAND() LIMIT " . $n;
 
         $sql = "SELECT * FROM `werk` WHERE categorie=:category" . $limit;
 
@@ -75,7 +101,7 @@ class Custom
     {
         if ($_SERVER['REQUEST_METHOD'] === "GET" && $this->isValidCategory($_GET['c'] ?? false)) {
 
-            
+
             return $_GET['c'];
         } else if ($_SERVER['REQUEST_METHOD'] === "POST" && $this->isValidCategory($_POST['c'] ?? false)) {
 
@@ -90,15 +116,15 @@ class Custom
     {
 
         //mind that id from $_GET is a string, not an int.
-        $result = match ( $value ) {
-            
+        $result = match ($value) {
+
             CAT_KEY_NEW => true,
             CAT_KEY_APPLIED => true,
-            CAT_KEY_ARCHIVE => true, 
+            CAT_KEY_ARCHIVE => true,
             CAT_KEY_AUTONOMOUS => true,
             default => false
         };
-        
+
         return $result;
     }
 }
